dd81654bdaee071f575648bdcf1b0a0e5df24ce8
Gabden <Gabdne5545@gmail.com>
2019-08-07 23:08:27 +0300

    Create web part

--- senator-shop-web/src/main/java/ru/ryazan/senatorshop/web/controllers/MainController.java
+++ senator-shop-web/src/main/java/ru/ryazan/senatorshop/web/controllers/MainController.java
@@ -2,8 +2,13 @@ package ru.ryazan.senatorshop.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.ryazan.senatorshop.core.domain.Product;
 import ru.ryazan.senatorshop.core.service.ProductService;
+import ru.ryazan.senatorshop.web.exceptions.NotFoundException;
+
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -23,5 +28,14 @@ public class MainController {
     public String productList(Model model){
         model.addAttribute("products", productService.findAll());
         return "product-list";
+    }
+
+    @RequestMapping("/productList/product/{id}")
+    public String product(@PathVariable Long id, Model model){
+        Optional<Product> productFromDB = Optional.ofNullable(productService.findById(id)
+                .filter(product -> product.getId().equals(id))
+                .orElseThrow(NotFoundException::new));
+        model.addAttribute("product", productFromDB.get());
+        return "product";
     }
 }
