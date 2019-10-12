63c2d18eed0ca17a7fc54b62b435e13026cd969a
Gabden <Gabdne5545@gmail.com>
2019-10-12 14:49:57 +0300

    Carousel refactor

--- senator-shop-web/src/main/java/ru/ryazan/senatorshop/web/controllers/ProductController.java
+++ senator-shop-web/src/main/java/ru/ryazan/senatorshop/web/controllers/ProductController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import ru.ryazan.senatorshop.core.domain.Product;
-import ru.ryazan.senatorshop.core.service.CartService;
 import ru.ryazan.senatorshop.core.service.ProductService;
 import ru.ryazan.senatorshop.web.exception.MyFileNotFoundException;
 
@@ -33,5 +33,14 @@ public class ProductController {
                 .orElseThrow(MyFileNotFoundException::new));
         model.addAttribute("product", productFromDB.get());
         return "product";
+    }
+
+    @RequestMapping("/productList/product/api/{id}")
+    @ResponseBody
+    public   Product productApi(@PathVariable Long id){
+        Optional<Product> productFromDB = Optional.ofNullable(productService.findById(id)
+                .filter(product -> product.getId().equals(id))
+                .orElseThrow(MyFileNotFoundException::new));
+        return productFromDB.get();
     }
 }
