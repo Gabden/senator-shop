2a4e0171bb1a851063211956b9c64f9723515c64
Gabden <Gabdne5545@gmail.com>
2020-02-15 08:10:20 +0300

    Go to top button

--- senator-shop-core/src/main/resources/static/js/home.js
+++ senator-shop-core/src/main/resources/static/js/home.js
@@ -1,5 +1,27 @@
 //-------Navigation bar collapse ------------
 $(document).ready(function () {
+    //Get the button:
+    var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        scrollFunction()
+    };
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    $('#myBtn').on('click', function () {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    });
+
+
     $("#sidebar").mCustomScrollbar({
         theme: "minimal"
     });
