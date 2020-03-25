142b383a6dc0128609b52b4a0e5e350655467bd8
Gabden <Gabdne5545@gmail.com>
2020-03-25 22:36:42 +0300

    Sending order email

---
+++ senator-shop-core/src/main/java/ru/ryazan/senatorshop/core/mail/OrderItem.java
@@ -0,0 +1,58 @@
+package ru.ryazan.senatorshop.core.mail;
+
+public class OrderItem {
+    private Long productId;
+    private String finalPrice;
+    private int quantity;
+    private String productName;
+
+    public OrderItem(Long productId, String finalPrice, int quantity, String productName) {
+        this.productId = productId;
+        this.finalPrice = finalPrice;
+        this.quantity = quantity;
+        this.productName = productName;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(String finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "OrderTemplateData{" +
+                "productId=" + productId +
+                ", finalPrice='" + finalPrice + '\'' +
+                ", quantity=" + quantity +
+                ", productName='" + productName + '\'' +
+                '}';
+    }
+}
--- senator-shop-core/src/main/java/ru/ryazan/senatorshop/core/mail/SendgridEmailSender.java
+++ senator-shop-core/src/main/java/ru/ryazan/senatorshop/core/mail/SendgridEmailSender.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 import ru.ryazan.senatorshop.core.domain.cart.Cart;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class SendgridEmailSender implements EmailService {
@@ -34,23 +36,24 @@ public class SendgridEmailSender implements EmailService {
 
     @Override
     public void sendRegistrationEmail(String mailTo) {
-        sendMail(mailRegistrationTemplateId, mailTo, "");
+        sendMail(mailRegistrationTemplateId, mailTo, "", null, 0L);
     }
 
     @Override
     public void sendOrderEmail(Cart cart, Long orderId) {
-
+        sendMail(mailOrderTemplateId, cart.getCustomer().getCustomerName(), "", cart, orderId);
+        sendMail(mailOrderTemplateId, adminMail, "", cart, orderId);
     }
 
     @Override
     public void sendRestoreMail(String mail, String password) {
-        sendMail(mailChangePasswordTemplateId, mail, password);
+        sendMail(mailChangePasswordTemplateId, mail, password, null, 0L);
     }
 
 
-    public void sendMail(String templateId, String mailTo, String newPassword) {
+    public void sendMail(String templateId, String mailTo, String newPassword, Cart cart, Long orderId) {
         SendGrid sendGrid = new SendGrid(apiKey);
-        Mail mail = prepareMail(templateId, mailTo, newPassword);
+        Mail mail = prepareMail(templateId, mailTo, newPassword, cart, orderId);
         Request request = new Request();
         request.setMethod(Method.POST);
         request.setEndpoint("mail/send");
@@ -65,13 +68,31 @@ public class SendgridEmailSender implements EmailService {
 
     }
 
-    private Mail prepareMail(String templateId, String mailTo, String newPassword) {
+    private Mail prepareMail(String templateId, String mailTo, String newPassword, Cart cart, Long orderId) {
         Mail mail = new Mail();
-        Email from = new Email("senator24a@gmail.com");
+        if (mailTo.equals(adminMail)) {
+            Email from = new Email("service@senator-wine.ru");
+            mail.setFrom(from);
+        } else {
+            Email from = new Email(adminMail);
+            mail.setFrom(from);
+        }
+
         Email to = new Email(mailTo);
-        mail.setFrom(from);
+
         Personalization personalization = new Personalization();
         personalization.addDynamicTemplateData("password", newPassword);
+        if (cart != null) {
+            List<OrderItem> orderItems = new ArrayList<>();
+            cart.getCartItems().forEach(cartItem -> {
+                OrderItem orderItem = new OrderItem(cartItem.getProduct().getId(), cartItem.getCartItemFinalPrice(), cartItem.getQuantity(), cartItem.getProduct().getProductName());
+                orderItems.add(orderItem);
+            });
+            personalization.addDynamicTemplateData("orderItems", orderItems);
+            personalization.addDynamicTemplateData("grandTotal", cart.getGrandTotal());
+            personalization.addDynamicTemplateData("orderId", orderId);
+            personalization.addDynamicTemplateData("phone", cart.getCustomer().getCustomerPhone());
+        }
         personalization.addTo(to);
         mail.addPersonalization(personalization);
 
