de93debe35a3ecdde37922cf3055d6a5dab73ef2
Gabden <Gabdne5545@gmail.com>
2020-05-09 15:12:21 +0300

    Navbar and footer skeleton

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <v-app>
-    <Login />
+    <Navbar />
+    <v-content>
+      <Login />
+    </v-content>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import Login from "@/views/Login.vue";
+import Navbar from "@/components/Navbar.vue";
+import Footer from "@/components/Footer.vue";
 export default {
   name: "App",
   components: {
-    Login
+    Login,
+    Navbar,
+    Footer
   }
 };
 </script>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app-bar app dark color="info">
+    <v-toolbar-title>Senator</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text rounded>Home</v-btn>
+    <v-btn text rounded>Secured</v-btn>
+    <v-btn text rounded>Only for admin</v-btn>
+    <v-btn text rounded>Login</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
