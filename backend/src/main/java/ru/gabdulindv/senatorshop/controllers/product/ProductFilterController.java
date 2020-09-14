6e07028083dcc47151e4d64a9dea2895dddcf399
Gabden <Gabdne5545@gmail.com>
2020-09-14 12:11:50 +0300

    filter controllers addition 2

--- backend/src/main/java/ru/gabdulindv/senatorshop/controllers/product/ProductFilterController.java
+++ backend/src/main/java/ru/gabdulindv/senatorshop/controllers/product/ProductFilterController.java
@@ -1,14 +1,14 @@
 package ru.gabdulindv.senatorshop.controllers.product;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.gabdulindv.senatorshop.model.product.Filter;
 import ru.gabdulindv.senatorshop.model.product.Product;
 import ru.gabdulindv.senatorshop.service.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -19,10 +19,179 @@ public class ProductFilterController {
         this.productService = productService;
     }
 
-    @RequestMapping(value = "/filter/category", method = RequestMethod.POST)
+    @RequestMapping(value = "/filter", method = RequestMethod.POST)
     public ResponseEntity filterProductsCategory(@RequestParam(value = "page", defaultValue = "0") int page, @RequestBody Filter filter) {
+        if (page < 0) {
+            page = 0;
+        }
         Pageable pageable = PageRequest.of(page, 8, Sort.by("productId").descending());
-        Page<Product> products = productService.findProductsByProductCategoryIn(filter.getSelectedCategories(), pageable);
+        Page<Product> products;
+
+        boolean hasCategory = filter.getSelectedCategories() != null && filter.getSelectedCategories().size() > 0;
+        boolean hasType = filter.getSelectedTypes() != null && filter.getSelectedTypes().size() > 0;
+        boolean hasCountry = filter.getSelectedCountry() != null && filter.getSelectedCountry().length() > 0;
+        boolean hasManufacturer = filter.getSelectedManufacturer() != null && filter.getSelectedManufacturer().length() > 0;
+
+        List<Product> filterProducts = new ArrayList<>();
+        if (hasCategory && !hasType && !hasCountry && !hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+        }
+
+        if (hasCategory && hasType && !hasCountry && !hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+        }
+
+        if (hasCategory && !hasType && hasCountry && !hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            filterProducts.addAll(countryProducts);
+        }
+
+        if (hasCategory && !hasType && !hasCountry && hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+        if (!hasCategory && hasType && hasCountry && !hasManufacturer) {
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            filterProducts.addAll(countryProducts);
+        }
+
+        if (!hasCategory && hasType && !hasCountry && !hasManufacturer) {
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+        }
+
+        if (!hasCategory && hasType && !hasCountry && hasManufacturer) {
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+        if (!hasCategory && !hasType && hasCountry && !hasManufacturer) {
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            filterProducts.addAll(countryProducts);
+        }
+
+        if (!hasCategory && !hasType && hasCountry && hasManufacturer) {
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(countryProducts);
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+        if (!hasCategory && !hasType && !hasCountry && hasManufacturer) {
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+        if (hasCategory && hasType && hasCountry && !hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            filterProducts.addAll(countryProducts);
+        }
+
+        if (hasCategory && hasType && !hasCountry && hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+        if (hasCategory && !hasType && hasCountry && hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            filterProducts.addAll(countryProducts);
+
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+        if (!hasCategory && hasType && hasCountry && hasManufacturer) {
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            filterProducts.addAll(countryProducts);
+
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+        if (hasCategory && hasType && hasCountry && hasManufacturer) {
+            for (String category : filter.getSelectedCategories()) {
+                List<Product> foundedProducts = productService.findProductsByProductCategoryContains(category);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            for (String type : filter.getSelectedTypes()) {
+                List<Product> foundedProducts = productService.findProductsByProductDetails_ProductTypeContains(type);
+                filterProducts.addAll(foundedProducts);
+            }
+
+            List<Product> countryProducts = productService.findProductsByProductDetails_ProductCountryContains(filter.getSelectedCountry());
+            filterProducts.addAll(countryProducts);
+
+            List<Product> manufacturerProducts = productService.findProductsByProductDetails_ProductManufacturerContains(filter.getSelectedManufacturer());
+            filterProducts.addAll(manufacturerProducts);
+        }
+
+
+        int start = (int) pageable.getOffset();
+        int end = (int) (start + pageable.getPageSize()) > filterProducts.size() ? filterProducts.size() : (start + pageable.getPageSize());
+        products = new PageImpl<>(filterProducts.subList(start, end), pageable, filterProducts.size());
+
+
         return ResponseEntity.ok(products);
     }
 
